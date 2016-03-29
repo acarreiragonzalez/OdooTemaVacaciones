46b5c0e74bc0d0a43c685ab340ccf091e332ccc8
acarreiragonzalez <acarreiragonzalez@atanasoft19.danielcastelao.org>
2016-03-29 12:17:05 +0200

    Todo funciona correcto, texto e imaxen implantada

---
+++ theme_angel/__openerp__.py
@@ -0,0 +1,14 @@
+{
+    'name':'Tutorial Jai',
+    'description': 'Catoire tema',
+    'version':'1.0',
+    'author':'Catoire',
+
+    'data': [
+    'views/assets.xml',
+    'views/layout.xml',
+    'views/pages.xml'
+],
+    'category': 'Theme/Creative',
+    'depends': ['website'],
+}
